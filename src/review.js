e86aef073158fad8db40f5c1c909519ce648bee1
Derek <xuderekq@hotmail.com>
2017-07-17 07:45:05 +0800

    Added more reviews

--- src/review.js
+++ src/review.js
@@ -4,5 +4,9 @@ export default {
     '2B87zXm9bOWvAJdkJBTpzF' :
         "Lorde's newest album is full of emotional performances and chilling lyrics. As before, Lorde is able to create beautiful atmospheres through the use of very simple, but impactful instrumental backings. Her lyrics are often very relatable, allowing her to connect deeply with the listener. Despite the sad, reflective music though, this album is still incredibly catchy",
     '0dAMC0nNikIjhD8LeRZfhH' :
-        "Bonito Generation is a wonderful pop music album. It features amazingly positive, fun, lighthearted lyrics. KKB has a special sound, mixing Japanese style synth instrumentals with vocals in both English and Japanese. The songs are so catchy and deliver such heartwarming messages, you can't help but sing along!"
+        "Bonito Generation is a wonderful pop music album. It features amazingly positive, fun, lighthearted lyrics. KKB has a special sound, mixing Japanese style synth instrumentals with vocals in both English and Japanese. The songs are so catchy and deliver such heartwarming messages, you can't help but sing along!",
+    '0xtTojp4zfartyGtbFKN3v' :
+        "Fleet Foxes' newest album is wonderful. As before, they use an extremely large variety of instruments and styles, each perfectly suiting the atmosphere of the song. Each song can undergo drastic changes in volume and style. The songs feature the elaborate folk metaphors that have come from Fleet Foxes in the past. The songwriting is brilliant. They deal with serious, sensitive matters such as police brutality, how people treat one another and the future of the world.",
+    '7dxKtc08dYeRVHt3p9CZJn' :
+        "Many view OK Computer as the best album ever created and it is certainly one of my favourites. It features songs with great lyrics and captivating instrumental atmospheres. The haunting vocals cut through the simple - but beautiful - instrumentals and create an extremely enjoyable experience. Songs vary in subject matter but each is an equally chilling performance."
 }
